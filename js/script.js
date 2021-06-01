a008c988d5b2803501b18a695f5c51ff03ee876a
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-02 00:19:37 +0300

    add addition function.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 function add(number1, number2) {
   return number1 + number2;
-  }
-  
-  let result = add(2, 3);
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}    
+  // let result = add(2, 3);
   
   // alert(result);
   
   let number1 = parseInt(prompt("Enter a number:"));
   let number2 = parseInt(prompt("Enter another number"));
   
-  alert(add(number1, number2));
+  let result = subtract(number1, number2);
+  alert(result);
