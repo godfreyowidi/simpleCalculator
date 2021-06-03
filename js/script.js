dd2c76858115dd38c00331a46997bc599d43ff93
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-04 01:24:04 +0300

    WIP: modifying the scripts.

--- js/script.js
+++ js/script.js
@@ -20,5 +20,14 @@ function divide(number1, number2) {
   let number1 = parseInt(prompt("Enter a number:"));
   let number2 = parseInt(prompt("Enter another number"));
   
-  let result = divide(number1, number2);
-  alert(result);
+alert(add(number1, number2));
+
+$document.ready(function(){
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("add1").val());
+    const number2 = parseInt($("add2").val());
+    const result = add(number1 + number2);
+    alert(result);
+  });
+});
