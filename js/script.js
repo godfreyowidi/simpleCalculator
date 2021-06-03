78d546f5bdaabc4388c10b2e9f327c267b4c3943
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-04 02:29:25 +0300

    add branching for different operators.

--- js/script.js
+++ js/script.js
@@ -25,10 +25,19 @@ function divide(number1, number2) {
 $(document).ready(function(){
   $("form#calculator").submit(function(event) {
     event.preventDefault();
-    const number1 = parseInt($("number1").val());
-    const number2 = parseInt($("number2").val());
-    const result = add(number1 + number2);
-    const operator = $("input:radio[name=operator]:checked")
+    const number1 = parseInt($("#number1").val());
+    const number2 = parseInt($("#number2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
   })
 })
