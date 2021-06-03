af5fd20f96f39c6b49746446eb57f87f949c3f16
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-04 02:42:10 +0300

    add final code after refactor.

--- js/script.js
+++ js/script.js
@@ -13,15 +13,7 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
   return number1 / number2;
 }    
-  // let result = add(2, 3);
-  
-  // alert(result);
-  
-  // let number1 = parseInt(prompt("Enter a number:"));
-  // let number2 = parseInt(prompt("Enter another number"));
-  
-//alert(add(number1, number2));
-
+ 
 $(document).ready(function(){
   $("form#calculator").submit(function(event) {
     event.preventDefault();
